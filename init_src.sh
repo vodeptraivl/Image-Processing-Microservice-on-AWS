7d840c05662ca193767dcce957591a14a5fdfe11
VOLA <admin@MacBook-Pro-cua-Eudy.local>
2024-08-26 13:34:21 +0700

    init

---
+++ init_src.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/udacity/cd12099-Full-Stack-Apps-AWS
---
+++ install_aws.sh
@@ -0,0 +1,5 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip
